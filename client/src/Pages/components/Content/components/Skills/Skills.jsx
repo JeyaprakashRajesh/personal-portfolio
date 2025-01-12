6400678aaf73344cd37f3d80f707d6054900f1fd
JeyaprakashRajesh <r.jeya2005@gmail.com>
2025-01-12 08:08:05 +0530

    changes in skills

--- client/src/Pages/components/Content/components/Skills/Skills.jsx
+++ client/src/Pages/components/Content/components/Skills/Skills.jsx
@@ -42,7 +42,7 @@ const content = [
 function renderContent() {
     return content.map((tile, index) => (
         <div key={index} className="skill-tile">
-            {tile.heading}
+            
         </div>
     ));
 }
